776d92f2ad77828988d6000be753ee56d20fd84c
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-04-03 13:28:43 +0530

    Update pdf.py
    added a Directory to to scan all pdf placed there

--- elton/pdf_text/pdf.py
+++ elton/pdf_text/pdf.py
@@ -1,3 +1,4 @@
+import os
 import fitz
 
 def extract_text_from_pdf(pdf_path):
@@ -8,6 +9,19 @@ def extract_text_from_pdf(pdf_path):
             text += page.get_text()
     return text
 
-# Replace "your_pdf_file.pdf" with the path to your PDF file
-pdf_text = extract_text_from_pdf("test2.pdf")
-print(pdf_text)
+def extract_text_from_pdfs_in_folder(folder_path):
+    folder_contents = os.listdir(folder_path)
+    pdf_files = [file for file in folder_contents if file.endswith(".pdf")]
+    all_texts = []
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(folder_path, pdf_file)
+        text = extract_text_from_pdf(pdf_path)
+        all_texts.append(text)
+    return all_texts
+
+# Replace "Data" with the path to your folder containing PDF files
+folder_texts = extract_text_from_pdfs_in_folder("Data")
+
+# Printing texts from all PDFs in the folder
+for text in folder_texts:
+    print(text)
