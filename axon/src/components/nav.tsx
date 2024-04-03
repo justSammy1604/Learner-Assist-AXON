cc5c6dfd7a62e94181eba1e3c8abf67695b135c3
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-04-03 19:07:14 +0530

    gg

--- axon/src/components/nav.tsx
+++ axon/src/components/nav.tsx
@@ -1,13 +1,18 @@
 'use client'
 import { signOut } from "next-auth/react";
 import { Button } from "./ui/button";
-
+import { IoHome } from "react-icons/io5";
+import { FaSearch } from "react-icons/fa";
+import { FaTrophy } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
 
 export default function Nav(){
-    return <div>
-
-        <Button  onClick={()=>signOut()}>
-            Signout
-        </Button>
-    </div>
+    return<>
+        <div className="w-100 h-100 bg-black-100 flex justify-evenly p-6">
+            <button><IoHome /></button>
+            <button><FaSearch /></button>
+            <button><FaTrophy /></button>
+            <button><FaUserCircle /></button>
+        </div>
+        </>
 }
