fb12f02630274cac7f2ba37f769ec10308541877
Terrence Braganca <113404658+Terrence31@users.noreply.github.com>
2024-04-04 11:20:58 +0530

    stuff

--- axon/src/app/(Main)/page.tsx
+++ axon/src/app/(Main)/page.tsx
@@ -12,35 +12,65 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import useGameStore from "@/components/state";
 import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { config } from "@/config";
 
 
 
 
 export default function Home() {
-  const router=useRouter()
+  const router = useRouter()
+  const [searchCourse, setSearchCourse] = useState<string | null>(null)
   const [currentCourse, setCurrentCourse] = useState<string | null>(null)
-  const {playerLives}=useGameStore()
-  useEffect(() => {
-    setCurrentCourse(localStorage.getItem('currentCourse'))
-  }, [])
+  const [searchQuery, setSearchQuery] = useState<string>("")
+  const [isFetching, setIsFetching] = useState(false)
+  const [isPresent, setIsPresent] = useState(0)
+  const [file, setFile] = useState<File | null>(null)
+  const [courses, setCourses] = useState<Array<{
+    name: string,
+    code: string
+  }> | false>([])
+  const { playerLives } = useGameStore()
+  const getCourses = () => {
+    const fd = new FormData()
+
+    if (file !== null) {
+      fd.append('file', file)
+
+    }
+    setIsFetching(true)
+    fetch(`${config.server}/search?` + new URLSearchParams({
+      query: searchQuery
+    })
+      , {
+        method: "POST",
+        body: fd
+      })
+      .then(resp => resp.json())
+      .then(d => {
+        setIsPresent(d.present)
+        setCourses(d.courses)
+        console.log(d)
+        setIsFetching(false)
+      })
+
+  }
   return (
     <main className="p-8 ">
-      <h1 className="text-3xl font-bold">{currentCourse}</h1>
-
-      <div className="mt-2  pb-12">
-        <h3 className="font-medium ">Topics You Will Learn</h3>
-        <ul className=" text-sm ">
-          <li>
-            Topic 1
-          </li>
-          <li>
-            Topic 2
-          </li>
-        </ul>
-
-      </div>
       <div>
-        <Button disabled={playerLives===0} onClick={()=>router.push('/Quiz/2132')} className="w-full py-3 bg-orange-400  h-16 text-lg p-4 rounded-md hover:bg-orange-300  ">Start Quiz</Button>
+        <Label className="text-2xl mb-6">Choose Course</Label>
+        <div className="flex gap-2 mt-2">
+          <Input className="h-16 mb-2" value={searchQuery as string} onChange={(e) => setSearchQuery(e.target.value)} />
+          <Input className="h-16 mb-2" type="file" accept=".txt" onChange={(e) => setFile(e.target.files && e.target.files[0] as File)} />
+
+        </div>
+        <div className=" flex flex-wrap gap-1">
+          {courses && courses.map(i => <Button type="button" className="block " onClick={() => setCurrentCourse(i.name)}>{i.name}</Button>)}
+        </div>
+        <Button disabled={(playerLives === 0)&&(file===null)} onClick={() => currentCourse === null ? getCourses() : router.push(`/Quiz/${currentCourse}`)} className="w-full py-3 bg-orange-400  h-16 text-lg p-4 rounded-md hover:bg-orange-300  mt-4 ">
+          {currentCourse === null ? "Search" : `Start ${currentCourse} Quiz`}
+        </Button>
       </div>
     </main>
   );
